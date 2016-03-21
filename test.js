55751e6ceda491bbf9fc98a7127b35b09c602556
Thomas <pubthomaspoujol@gmail.com>
2016-03-21 16:55:26 +0100

    Ah ça fait du bien de réecrire un peu de code

---
+++ test.js
@@ -0,0 +1,47 @@
+var schedule = require('node-schedule');
+var request = require('request')
+
+
+
+var j = schedule.scheduleJob('0 * * * * *', function() {
+  getPoints();
+});
+
+
+
+function getPoints() {
+  var dataset = [];
+  var url = 'http://opendata.paris.fr/api/records/1.0/search/?dataset=stations-velib-disponibilites-en-temps-reel&rows=3000&facet=banking&facet=bonus&facet=status&facet=contract_name';
+  request(url, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      body = JSON.parse(body);
+      var rows = body.records;
+
+      for (var i = 0; i < rows.length; i++) {
+        var current = rows[i];
+        if (current.fields.status == "CLOSED") {
+          dataset.push({
+            open: false,
+            location: {
+              Lat: current.geometry.coordinates[1],
+              Lng: current.geometry.coordinates[0]
+            },
+            weight: 0
+          });
+          continue;
+        }
+
+        dataset.push({
+          open: true,
+          location: {
+            Lat: current.geometry.coordinates[1],
+            Lng: current.geometry.coordinates[0]
+          },
+          weight: current.fields.available_bikes
+        });
+      }
+      console.log('We have all the data we need for this time : ' + dataset.length);
+    }
+
+  });
+}
