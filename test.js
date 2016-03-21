ea5c7d55da0665c4ce5ffb4662c8691603ff4bed
Thomas <pubthomaspoujol@gmail.com>
2016-03-21 17:45:35 +0100

    divided into two tables now we need to setup the DB

--- test.js
+++ test.js
@@ -10,36 +10,48 @@ var j = schedule.scheduleJob('0 * * * * *', function() {
 
 
 function getPoints() {
-  var dataset = [];
+  console.log("on lance la requête \n")
+  var stations = [];
+  var filling = [];
   var url = 'http://opendata.paris.fr/api/records/1.0/search/?dataset=stations-velib-disponibilites-en-temps-reel&rows=3000&facet=banking&facet=bonus&facet=status&facet=contract_name';
   request(url, function(error, response, body) {
+    console.log("C'est parti mon kiki " + response.statusCode + "\n");
+    var name;
     if (!error && response.statusCode == 200) {
       body = JSON.parse(body);
       var rows = body.records;
 
       for (var i = 0; i < rows.length; i++) {
         var current = rows[i];
+
+        id = current.fields.name.split(" ", 1);
+
+        stations.push({
+          id: id[0],
+          Lat: current.geometry.coordinates[1],
+          Lng: current.geometry.coordinates[0]
+        });
+
         if (current.fields.status == "CLOSED") {
-          dataset.push({
+
+          filling.push({
             open: false,
-            Lat: current.geometry.coordinates[1],
-            Lng: current.geometry.coordinates[0],
+            station_id: id[0],
             stands: 0,
             bikes: 0
           });
           continue;
         }
 
-        dataset.push({
+        filling.push({
           open: true,
-          Lat: current.geometry.coordinates[1],
-          Lng: current.geometry.coordinates[0],
-          stands: current.available_bikes_stands,
+          station_id: id[0],
+          stands: current.fields.available_bikes_stands,
           bikes: current.fields.available_bikes
 
         });
       }
-      console.log('We have all the data we need for this time : ' + dataset.length);
+      console.log(filling);
     }
 
   });
