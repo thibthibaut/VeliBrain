163306dfaaea8d4922399f89d4d90072648db88d
Thomas <pubthomaspoujol@gmail.com>
2016-03-22 13:55:38 +0100

    Storing in database working

--- app.js
+++ app.js
@@ -8,12 +8,17 @@ var conString = "postgres://thomas:test@localhost/velib";
 
 
 
-var j = schedule.scheduleJob('0 * * * * *', function() {
+var job = schedule.scheduleJob('0 * * * * *', function() {
   getPoints();
 });
 
 
 
+/**
+ * getPoints - function that retriev all the data from open data paris and then dispach the interresting info into two different arrays
+ *
+ * @return {type}  none
+ */
 function getPoints() {
   log.debug("On lance la requête \n");
   var stations = [];
@@ -23,9 +28,9 @@ function getPoints() {
     log.debug("C'est parti mon kiki " + response.statusCode + "\n");
     var name;
     if (!error && response.statusCode == 200) {
-      var date = moment(new Date()).format('YYYY-MM-DD HH:mm:ss Z');//WHY NOT A UNIX TIME STAMP ?
-      //var date = new Date().getTime(); 
-      //Number of miliseconds since January, first, 1970. On a 32/64 bits integer 
+      var date = moment(new Date()).format('YYYY-MM-DD HH:mm:ss Z'); //WHY NOT A UNIX TIME STAMP ?
+      //var date = new Date().getTime();
+      //Number of miliseconds since January, first, 1970. On a 32/64 bits integer
       //(Could be parsed to 32 bits int for database storage) => see ToInt32 method
 
       body = JSON.parse(body);
@@ -35,7 +40,7 @@ function getPoints() {
         var current = rows[i];
 
         id = current.fields.name.split(" ", 1)[0];
-      
+
         stations.push({
           id: id,
           lat: current.geometry.coordinates[1],
@@ -57,12 +62,12 @@ function getPoints() {
         filling.push({
           open: true,
           station_id: id,
-          stands: current.fields.available_bikes_stands,
+          stands: current.fields.available_bike_stands,
           bikes: current.fields.available_bikes,
           date
         });
       }
-      log.info(date + ' information saved, ' + filling.length + ' lines recorded');
+      log.info('Information retrieved, ' + filling.length + ' lines recorded for date: ' + date);
       saveDB(stations, filling);
     }
   });
@@ -78,35 +83,42 @@ function getPoints() {
  * @return void                no return
  */
 function saveDB(array_stations, array_filling) {
-  var i;
+  var i, j;
   var client = new pg.Client(conString);
-  console.log("on arrive jusque là");
+  var sql;
+
   client.connect(function(err) {
     if (err) {
       return console.error('could not connect to postgres', err);
     }
+
     client.query('select * from station', function(err, result) {
+
       if (err) {
         return console.error('error running query', err);
       }
-      var sql;
 
       if (result.rows.length == 0) {
-        for (i = 0; i < array_stations; i++) {
-          sql = `insert into station (lat, lng, id) values (${array_stations[i].lat},${array_stations[i].lng},${array_stations[i].id});`;
-          console.log(sql);
-          client.query();
+        for (i = 0; i < array_stations.length; i++) {
+          sql = `insert into station (lat, lng, id) values (${array_stations[i].lat},${array_stations[i].lng},\'${array_stations[i].id}\');`;
+          client.query(sql, function(err, res) {});
         }
       } else if (result.rows.length != array_stations.length) {
         //TODO gérer la comparaison et l'ajout de nouvelles stations
       }
 
-      for (i = 0; i < array_filling.length; i++) {
-        client.query(`insert into filling (open,station_id,stands,bikes,creation_date) values (${array_filling[i].open},${array_filling[i].station_id},${array_filling[i].stands},${array_filling[i].bikes},${array_filling[i].date});`)
+      for (j = 0; j < array_filling.length; j++) {
+        sql = `insert into filling (open,station_id,stands,bikes,creation_date) values (${array_filling[j].open},\'${array_filling[j].station_id}\',${array_filling[j].stands},${array_filling[j].bikes},\'${array_filling[j].date}\');`
+        //log.debug(sql);
+        if (j < array_filling.length - 1) {
+          client.query(sql);
+        } else {
+          client.query(sql, function(err, res) {
+            client.end();
+            log.info('All data are saved in DB');
+          });
+        }
       }
-
-      log.info('On est à la fin de save');
-      //client.end();
     });
   });
 
